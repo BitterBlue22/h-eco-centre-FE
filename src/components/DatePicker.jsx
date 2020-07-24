513698d789b9095783c72d2eadb8ec7e46ae0e2a
BitterBlue22 <telisax@gmail.com>
2020-07-24 21:02:29 +0100

    adds DatePicker component

---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
+export default function DatePickers({ label }) {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.container} noValidate>
+      <TextField
+        id="date"
+        variant="outlined"
+        label={label}
+        type="date"
+        defaultValue="2017-05-24"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+  );
+}
