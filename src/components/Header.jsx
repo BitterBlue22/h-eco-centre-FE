3d8503ad74373f5681cc0d73c2fe802c24e83da1
BitterBlue22 <telisax@gmail.com>
2020-07-24 16:09:41 +0100

    updates navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, Button, makeStyles } from "@material-ui/core";
 import Navigation from "./Navigation";
 
+const useStyles = makeStyles((theme) => ({
+  root: { flexGrow: 0 },
+  toolbar: {
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  navbar: { flexGrow: 1 },
+  button: {},
+}));
+
 const Header = () => {
+  const classes = useStyles();
   return (
-    <AppBar position="sticky">
-      <Toolbar>
-        <Navigation />
+    <AppBar position="static" className={classes.root}>
+      <Toolbar className={classes.toolbar}>
+        <Navigation className={classes.navbar} />
+        <Button edge="end" className={classes.button}>
+          Register
+        </Button>
       </Toolbar>
     </AppBar>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Tabs, Tab } from "@material-ui/core";
+import { Link } from "@reach/router";
 
 function a11yProps(index) {
   return {
@@ -20,13 +21,39 @@ const Navigation = () => {
       onChange={handleChange}
       aria-label="navigation tabs"
     >
-      <Tab label="Home" {...a11yProps(0)} />
-      <Tab label="Our Vision" {...a11yProps(1)} />
-      <Tab label="Location" {...a11yProps(2)} />
-      <Tab label="Activities" {...a11yProps(3)} />
-      <Tab label="Renewable Energy" {...a11yProps(4)} />
-      <Tab label="Stakeholders" {...a11yProps(5)} />
-      <Tab label="Contact us" {...a11yProps(6)} />
+      <Tab label="Home" {...a11yProps(0)} to="/" component={Link} />
+      <Tab label="Our Vision" {...a11yProps(1)} to="/vision" component={Link} />
+      <Tab label="Location" {...a11yProps(2)} to="/location" component={Link} />
+      <Tab
+        label="Activities"
+        {...a11yProps(3)}
+        to="/activities"
+        component={Link}
+      />
+      <Tab
+        label="Renewable Energy"
+        {...a11yProps(4)}
+        to="/energy"
+        component={Link}
+      />
+      <Tab
+        label="Our Partners"
+        {...a11yProps(5)}
+        to="/partners"
+        component={Link}
+      />
+      <Tab
+        label="Volunteering"
+        {...a11yProps(6)}
+        to="/volunteer"
+        component={Link}
+      />
+      <Tab
+        label="Contact us"
+        {...a11yProps(7)}
+        to="/contact-us"
+        component={Link}
+      />
     </Tabs>
   );
 };
